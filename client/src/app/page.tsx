68068f126f3c0936a2998593812deb49a098eb85
tomasrobino <tomaserobino@gmail.com>
2023-08-20 17:27:32 +0200

    fixed topRated images request

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,18 +1,19 @@
+import { AxiosResponse } from 'axios';
 import LargeDisplay from './LargeDisplay'
 import styles from './page.module.css'
 import { getNewest, getTopRated } from './requests';
 
 
 export default async function Home() {
-  const topRated = await getTopRated();
-  const newest = await getNewest();
-
+  const topRated: AxiosResponse<any, any>[] = await getTopRated() as AxiosResponse<any, any>[];
+  const newest: AxiosResponse<any, any> = await getNewest() as AxiosResponse<any, any>;
+  console.log(await topRated[0].data.results[0]);
 
   return (
     <div className={styles.page}>
       <p>{}</p>
       <h1>Trending</h1>
-      <LargeDisplay {...{toDisplay: topRated?.data.results}}/>
+      <LargeDisplay {...{toDisplay: topRated[0].data.results}}/>
       <h1>New Releases</h1>
       <LargeDisplay {...{toDisplay: newest?.data.results}}/>
     </div>
--- client/src/app/requests.ts
+++ client/src/app/requests.ts
@@ -11,6 +11,18 @@ export async function getTopRated() {
         exclude_additions: true,
         key: "365861aa44ed48888609ad4668f3e3e2"
       }
+    }).then(async (res) => {
+      const gameArr = [];
+      for (let i = 0; i < 10; i++) {
+        gameArr.push(await axios.get(`https://api.rawg.io/api/games/${res.data.results[i].slug}/screenshots`, {
+          params: {
+            page: 1,
+            page_size: 5,
+            key: "365861aa44ed48888609ad4668f3e3e2"
+          }
+        }));
+      }
+      return gameArr;
     })
     return response;
   } catch (error) {

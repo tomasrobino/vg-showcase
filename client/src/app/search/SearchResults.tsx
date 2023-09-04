fd6c8aaebef6b40f64c98df17e5a1749f998711d
tomasrobino <tomaserobino@gmail.com>
2023-09-04 09:56:25 +0200

    added fallback for when search results have no img

--- client/src/app/search/SearchResults.tsx
+++ client/src/app/search/SearchResults.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-
 import { Card, CardActionArea, CardContent, CardMedia, Typography } from "@mui/material"
 import { gameResponse } from "../types"
 import { useRouter } from "next/navigation";
@@ -16,18 +15,27 @@ export default function SearchResults(props: {results: gameResponse[]}) {
 
     const cards = [];
     for (let i = 0; i < props.results.length; i++) {
+        let imageToDisplay
+
+        if(props.results[i].background_image !== null) {
+            imageToDisplay = 
+                <CardMedia
+                    sx={{
+                        height: 100,
+                        width: 100
+                    }}
+                    image={props.results[i].background_image}
+                />
+        } else {
+            imageToDisplay = <Typography>No image</Typography>
+        }
+
         cards.push(
             <Card key={"card"+i}>
                 <CardActionArea
                     onClick={() => handleClick(props.results[i].slug)}
                 >
-                    <CardMedia
-                        sx={{
-                            height: 100,
-                            width: 100
-                        }}
-                        image={props.results[i].background_image}
-                    />
+                    {imageToDisplay}
                     <CardContent>
                         <Typography>
                             {props.results[i].name}

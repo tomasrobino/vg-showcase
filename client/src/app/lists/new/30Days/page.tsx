b11390861bd6bfea0d183af371467b7b6650b56b
tomasrobino <tomaserobino@gmail.com>
2023-09-16 00:41:09 +0200

    last 30 days releases request done

--- client/src/app/lists/new/30Days/page.tsx
+++ client/src/app/lists/new/30Days/page.tsx
@@ -1,6 +1,12 @@
-export default function Page() {
-    return (
-        <>
-        </>
+import ResultsAndPasser from "../../ResultsAndPasser";
+import getResults from "../../request";
+
+export default async function Page() {
+    const today = new Date();
+    const monthAgo = new Date(today.getTime() - 2629800000);
+    const response = await getResults({dates: `${monthAgo.getFullYear()}-${("0"+monthAgo.getMonth()).slice(-2)}-${("0"+monthAgo.getDate()).slice(-2)},${today.getFullYear()}-${("0"+today.getMonth()).slice(-2)}-${("0"+today.getDate()).slice(-2)}`, page: 1, ordering: "released"});
+
+    return(
+        <ResultsAndPasser {...{response: response, ifSearch: ""}}/>
     )
 }

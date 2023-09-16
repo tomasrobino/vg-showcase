9d7edf349a9d294fa3fc543f310a39b7e5cd07df
tomasrobino <tomaserobino@gmail.com>
2023-09-16 04:48:24 +0200

    all SideBar buttons working

--- client/src/app/SideBar.tsx
+++ client/src/app/SideBar.tsx
@@ -11,6 +11,14 @@ export default function SideBar() {
         router.push(`results?page=1&${route}`);
     }
 
+    function handleGenre(genre: string) {
+        router.push(`results?page=1&genres=${genre}&ordering=released`);
+    }
+
+    function handlePlatform(platform: string) {
+        router.push(`results?page=1&platforms=${platform}&ordering=released`);
+    }
+
     return (
         <div className={styles.sideBar}>
             <Typography className={styles.title}>New Releases</Typography>
@@ -18,39 +26,49 @@ export default function SideBar() {
                 <Button className={styles.element} onClick={() => {
                     const today = new Date();
                     const monthAgo = new Date(today.getTime() - 2629800000);
-                    handleClick(`dates=${monthAgo.getFullYear()}-${("0"+monthAgo.getMonth()).slice(-2)}-${("0"+monthAgo.getDate()).slice(-2)},${today.getFullYear()}-${("0"+today.getMonth()).slice(-2)}-${("0"+today.getDate()).slice(-2)}&ordering=released`);
+                    router.push(`results?page=1&dates=${monthAgo.getFullYear()}-${("0"+(monthAgo.getMonth()+1)).slice(-2)}-${("0"+monthAgo.getDate()).slice(-2)},${today.getFullYear()}-${("0"+(today.getMonth()+1)).slice(-2)}-${("0"+today.getDate()).slice(-2)}&ordering=released`);
                 }}>Last 30 Days</Button>
-                <Button className={styles.element} onClick={() => {handleClick("new/past")}}>This week</Button>
-                <Button className={styles.element} onClick={() => {handleClick("new/next")}}>Next week</Button>
-                <Button className={styles.element} onClick={() => {handleClick("new/calendar")}}>Release calendar</Button>
+                <Button className={styles.element} onClick={() => {
+                    const today = new Date();
+                    const weekAgo = new Date(today.getTime() - 604800000);
+                    router.push(`results?page=1&dates=${weekAgo.getFullYear()}-${("0"+(weekAgo.getMonth()+1)).slice(-2)}-${("0"+weekAgo.getDate()).slice(-2)},${today.getFullYear()}-${("0"+(today.getMonth()+1)).slice(-2)}-${("0"+today.getDate()).slice(-2)}&ordering=released`);
+                }}>This week</Button>
+                <Button className={styles.element} onClick={() => {
+                    const today = new Date();
+                    const nextWeek = new Date(today.getTime() + 604800000);
+                    router.push(`results?page=1&dates=${today.getFullYear()}-${("0"+(today.getMonth()+1)).slice(-2)}-${("0"+today.getDate()).slice(-2)},${nextWeek.getFullYear()}-${("0"+(nextWeek.getMonth()+1)).slice(-2)}-${("0"+nextWeek.getDate()).slice(-2)}&ordering=released`);
+                }}>Next week</Button>
             </div>
 
             <Typography className={styles.title}>Top</Typography>
             <div className={styles.group}>
-                <Button className={styles.element} onClick={() => {handleClick("top/year")}}>Best of the year</Button>
+                <Button className={styles.element} onClick={() => {
+                    const today = new Date();
+                    router.push(`results?page=1&dates=${today.getFullYear()}-01-01,${today.getFullYear()}-${("0"+(today.getMonth()+1)).slice(-2)}-${("0"+today.getDate()).slice(-2)}&ordering=released`);
+                }}>Best of the year</Button>
                 <Button className={styles.element} onClick={() => {handleClick("top/top250")}}>All time top 250</Button>
             </div>
 
-            <Button className={styles.title} onClick={() => {handleClick("genre")}}>Genres</Button>
+            <Button className={styles.title} onClick={() => {router.push("genres")}}>Genres</Button>
             <div className={styles.group}>
-                <Button className={styles.element} onClick={() => {handleClick("genre/action")}}>Action</Button>
-                <Button className={styles.element} onClick={() => {handleClick("genre/strategy")}}>Strategy</Button>
-                <Button className={styles.element} onClick={() => {handleClick("genre/rpg")}}>RPG</Button>
-                <Button className={styles.element} onClick={() => {handleClick("genre/shooter")}}>Shooter</Button>
-                <Button className={styles.element} onClick={() => {handleClick("genre/adventure")}}>Adventure</Button>
-                <Button className={styles.element} onClick={() => {handleClick("genre/puzzle")}}>Puzzle</Button>
-                <Button className={styles.element} onClick={() => {handleClick("genre/racing")}}>Racing</Button>
-                <Button className={styles.element} onClick={() => {handleClick("genre/sports")}}>Sports</Button>
+                <Button className={styles.element} onClick={() => {handleGenre("action")}}>Action</Button>
+                <Button className={styles.element} onClick={() => {handleGenre("strategy")}}>Strategy</Button>
+                <Button className={styles.element} onClick={() => {handleGenre("rpg")}}>RPG</Button>
+                <Button className={styles.element} onClick={() => {handleGenre("shooter")}}>Shooter</Button>
+                <Button className={styles.element} onClick={() => {handleGenre("adventure")}}>Adventure</Button>
+                <Button className={styles.element} onClick={() => {handleGenre("puzzle")}}>Puzzle</Button>
+                <Button className={styles.element} onClick={() => {handleGenre("racing")}}>Racing</Button>
+                <Button className={styles.element} onClick={() => {handleGenre("sports")}}>Sports</Button>
             </div>
 
             <Button className={styles.title}>Platforms</Button>
             <div className={styles.group}>
-                <Button className={styles.element} onClick={() => {handleClick("platform/pc")}}>PC</Button>
-                <Button className={styles.element} onClick={() => {handleClick("platform/playstation4")}}>PlayStation 4</Button>
-                <Button className={styles.element} onClick={() => {handleClick("platform/xboxone")}}>Xbox One</Button>
-                <Button className={styles.element} onClick={() => {handleClick("platform/switch")}}>Nintendo Switch</Button>
-                <Button className={styles.element} onClick={() => {handleClick("platform/ios")}}>iOS</Button>
-                <Button className={styles.element} onClick={() => {handleClick("platform/android")}}>Android</Button>
+                <Button className={styles.element} onClick={() => {handlePlatform("4")}}>PC</Button>
+                <Button className={styles.element} onClick={() => {handlePlatform("187")}}>PlayStation 5</Button>
+                <Button className={styles.element} onClick={() => {handlePlatform("1")}}>Xbox One</Button>
+                <Button className={styles.element} onClick={() => {handlePlatform("7")}}>Nintendo Switch</Button>
+                <Button className={styles.element} onClick={() => {handlePlatform("3")}}>iOS</Button>
+                <Button className={styles.element} onClick={() => {handlePlatform("21")}}>Android</Button>
             </div>
         </div>
     )

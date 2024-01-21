f570fe4b41d8b0007604704d0953086086cb55a3
tomasrobino <tomaserobino@gmail.com>
2024-01-21 20:06:57 +0100

    Added FilterBar component with Select for ordering

---
+++ client/src/app/results/FilterBar.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import { InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React, { useCallback, useState } from "react";
+
+export default function FilterBar() {
+    const router = useRouter()
+    const pathname = usePathname()
+    const searchParams = useSearchParams()
+    const [ordering, setOrdering] = useState(searchParams.get("ordering") || "-relevance");
+   
+    // Get a new searchParams string by merging the current
+    // searchParams with a provided key/value pair
+    const createQueryString = useCallback(
+      (name: string, value: string) => {
+        const params = new URLSearchParams(searchParams.toString());
+        params.set(name, value);
+   
+        return params.toString();
+      },
+      [searchParams]
+    );
+
+    function handleSelection(e: SelectChangeEvent<string>) {
+        setOrdering(e.target.value);
+        router.push(pathname + '?' + createQueryString('ordering', e.target.value))
+    }
+   
+    return (
+        <div style={{
+            marginTop: "10px",
+            width: "100%"
+        }}>
+            <Select
+                onChange={handleSelection}
+                value={ordering}
+                label="Sort by"
+            >
+                
+                <MenuItem value="released">Newest</MenuItem>
+                <MenuItem value="-released">Oldest</MenuItem>
+                <MenuItem value="-rating">Rating</MenuItem>
+                <MenuItem value="name">Name</MenuItem>
+                <MenuItem value="-relevance">Relevance</MenuItem>
+            </Select>
+        </div>
+    )
+}
--- client/src/app/results/ResultsAndPasser.tsx
+++ client/src/app/results/ResultsAndPasser.tsx
@@ -1,12 +1,23 @@
+import { Suspense } from "react";
+import FilterBar from "./FilterBar";
 import PagePasser from "./PagePasser";
 import Results from "./Results";
 import styles from "./results.module.css"
 
 export default function ResultsAndPasser(props: {response: any}) {
     return(
-        <div className={styles.results}>
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center"
+        }}>
+            <Suspense>
+                <FilterBar/>
+            </Suspense>
+            <div className={styles.results}>
                 <Results {...{results: props.response.results}}/>
-                <PagePasser {...{next: props.response.next, prev: props.response.previous}}/>
+            </div>
+            <PagePasser {...{next: props.response.next, prev: props.response.previous}}/>
         </div>
     )
 }

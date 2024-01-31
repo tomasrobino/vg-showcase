3ba0550dfdce8803996fe9ef5ce798e21e904d7c
tomasrobino <tomaserobino@gmail.com>
2024-01-31 13:08:06 +0100

    added placeP function for parsing game desc

--- client/src/app/game/[slug]/page.tsx
+++ client/src/app/game/[slug]/page.tsx
@@ -10,12 +10,52 @@ export default async function Page({
     params: { slug: string}
 }) {
     const response: gameDetails = await getDetails({game: params.slug});
-    console.log(response.ratings);
+
+    function placeP(text: string): string {
+        let withinP: boolean = false;
+        let res: string = text;
+        let closingTag: boolean = false;
+        if (res[0] !== "<") {
+            res = "<p>" + res;
+            withinP = true;
+        }
+        let i = 1;
+        while (i < res.length) {
+            if (withinP && res[i] === "<" && res[i+3] !== "/") {
+                let part1: string = res.slice(0, i) + "</p>";
+                res = part1 + res.slice(i);
+                i+=4;
+                withinP = false;
+            } else if (closingTag) {
+                if (withinP === false && res[i] === ">" && i < res.length-1) {
+                    if (res[i+1] !== "<" && (res[i+1] !== " " || res[i+2] !== "<")) {
+                        let part1: string = res.slice(0, i+1) + "<p>";
+                        res = part1 + res.slice(i+1);
+                        i+=3;
+                        withinP = true;
+                        closingTag = false;
+                    }
+                }
+            } else if (res[i] === "<" && res[i+1] === "/") {
+                closingTag = true;
+            }
+            i++;
+        }
+        return res;
+    }
+
+    let description: Array<JSX.Element> = parse(placeP(response.description)) as Array<JSX.Element>;
+    description = description.map((element, i) => {
+        let CustomTag = `${element.type}` as keyof JSX.IntrinsicElements;
+        return (
+            <CustomTag key={i}>{element.props.children}</CustomTag>
+        )
+    });
 
     // <PieChart {...{data: [response.ratings[0].count, response.ratings[1].count, response.ratings[2].count, response.ratings[3].count]}}/>
 
     return(
-        <div id="aaa">
+        <div>
             <Typography>{response.released}</Typography>
             <Typography
                 className={styles.name}
@@ -30,10 +70,7 @@ export default async function Page({
                 className={styles.subtitle}
                 variant="h5"
             >About</Typography>
-            {parse(response.description)}
-            <div>
-
-            </div>
+            {description}
         </div>
     )
 }   
